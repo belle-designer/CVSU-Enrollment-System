ff8b6b82346beaa0d1084365fd90f424e2c1c0d4
Meann22 <120320327+Meann22@users.noreply.github.com>
2024-11-25 15:18:57 +0800

    initial commit

--- enrollment-system/src/components/Courses.jsx
+++ enrollment-system/src/components/Courses.jsx
@@ -2,18 +2,28 @@ import React from 'react';
 
 function Courses() {
   const courseData = [
+
     {
-      title: "Bachelor of Secondary Education",
+      title: "BS Computer Science",
+      description: "The study of computing concepts and theories, algorithmic foundations, and new development in computing."
+    },
+    {
+      title: "BS Information Technology",
+      description: "Includes the study of the utilization of both hardware and software technologies involving planning, installing, customizing, operating, managing, and maintaining information technology infrastructure."
+    },
+    {
+      title: "BS Secondary Education Major in Math",
+      description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    },
+    {
+      title: "BS Secondary Education Major in English",
       description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
     },
     {
       title: "BS Business Management",
       description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
     },
-    {
-      title: "BS Computer Science",
-      description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-    },
+  
     {
       title: "BS Criminology",
       description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
@@ -22,18 +32,13 @@ function Courses() {
       title: "BS Hospitality Management",
       description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
     },
-    {
-      title: "BS Criminology",
-      description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-    },
-    {
-      title: "BS Information Technology",
-      description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-    },
+
     {
       title: "BS Psychology",
       description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
     }
+
+    
   ];
 
   return (
@@ -47,7 +52,7 @@ function Courses() {
       {/* Courses Content */}
       <div className="courses-contents">
         {courseData.map((course, index) => (
-          <div className="category-item" key={index}>
+          <div className={`category-item ${index < 4 ? 'spaced-item' : ''}`} key={index}>
             <div className="category-desc">
               <h3>{course.title}</h3>
               <p>{course.description}</p>
